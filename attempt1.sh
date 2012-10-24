6e39c43f5070b80502810eeaa1b3bdd02e931d99
Brad Czerniak <ao5357@gmail.com>
2012-10-24 10:09:02 -0400

    schema.org and checklist items

--- attempt1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FILES=/Users/ao5357/Desktop/repos/ao5357.github.com/_posts/*
-for f in $FILES
-do
-  echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  /Users/ao5357/Desktop/repos/ao5357.github.com/html2text.py $f > $f.md
-done
